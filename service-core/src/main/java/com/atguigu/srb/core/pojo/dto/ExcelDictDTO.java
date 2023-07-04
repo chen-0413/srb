9c507b17a7d4d4042dbb4fcfcd11ad5f67c97726
Chen Xu Jiang <491839562@qq.com>
2023-07-04 20:30:35 +0800

    '数字字典的实体类'

---
+++ service-core/src/main/java/com/atguigu/srb/core/pojo/dto/ExcelDictDTO.java
@@ -0,0 +1,28 @@
+package com.atguigu.srb.core.pojo.dto;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
+
+/**
+ * @author :Chen Xu Jiang
+ * @date :  2023/7/4~20:27~星期二
+ * @description: 数字字典的实体类
+ */
+@Data
+public class ExcelDictDTO {
+
+    @ExcelProperty("id") // @ExcelProperty 让对应的属性在Excel中使用对应的名称
+    private Long id;
+
+    @ExcelProperty("上级id")
+    private Long parentId;
+
+    @ExcelProperty("名称")
+    private String name;
+
+    @ExcelProperty("值")
+    private Integer value;
+
+    @ExcelProperty("编码")
+    private String dictCode;
+}
